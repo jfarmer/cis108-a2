db8d752225e7e8368ff105b09f87f11091d1306a
Jason Zheng <jazheng@davidson.edu>
2020-03-25 14:16:07 -0400

    code from avgScrabbleScoreFromFile

--- src/avgScrabbleScoreFromFile.js
+++ src/avgScrabbleScoreFromFile.js
@@ -12,11 +12,20 @@ let avgScrabbleScore = require('./avgScrabbleScore');
  */
 function avgScrabbleScoreFromFile(fileName) {
   // This is your job. :)
+  let text = fs.readFileSync(`${fileName}`, 'utf-8');
+  let refinedText = '';
+  for (let element of text) {
+    if(element !== '\n' && element !== '\r') {
+      refinedText += element;
+    }
+  }
+  return avgScrabbleScore(refinedText);
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for avgScrabbleScoreFromFile:');
-
+  console.log('The avg of the test file is:', avgScrabbleScoreFromFile('test.txt'));
+  console.log('The avg of the sample-words file is:', avgScrabbleScoreFromFile('sample-words.txt'))
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
