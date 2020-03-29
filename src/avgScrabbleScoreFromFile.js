38c0bb738bfb65adc54e4626665e1b105937f812
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-03-29 13:34:36 +0200

    avg Scrabble Score From File

--- src/avgScrabbleScoreFromFile.js
+++ src/avgScrabbleScoreFromFile.js
@@ -11,11 +11,20 @@ let avgScrabbleScore = require('./avgScrabbleScore');
  * sample-words.txt.
  */
 function avgScrabbleScoreFromFile(fileName) {
-  // This is your job. :)
+  let fileWords = fs.readFileSync(fileName, 'utf-8');
+  let splittedFileWords = fileWords.split('\r\n');
+
+  let answer = avgScrabbleScore(splittedFileWords);
+  // console.log(splittedFileWords)
+  return answer;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for avgScrabbleScoreFromFile:');
+  console.log(avgScrabbleScoreFromFile('./sample-words.txt'));
+  console.log(avgScrabbleScoreFromFile('./words.txt') === 14);
+  console.log(avgScrabbleScoreFromFile('./wordsVol2.txt') === 12);
+  console.log(avgScrabbleScoreFromFile('./wordsVol3.txt') === 12.25);
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
